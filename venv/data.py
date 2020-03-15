54e16ffe5a11a7bf0710519ab94d163ff2072d2e
Maciej Ciura <maciej.ciura12@gmail.com>
2020-03-16 00:22:10 +0100

    Commit na dobranoc

--- venv/data.py
+++ venv/data.py
@@ -1,23 +1,48 @@
 import random
+import copy
+
 
 class Data:
-    tab=[]
-    dataSize = 0
-    hummPercentage = 0
+    tab=[]              #tablica z danymi
+    dataSize = 0        #długość ciągu bitów
+    hummPercentage = 0  #procentowa ilość błędów
 
+    random.seed()
     def __init__(self, dataSize, hummPercentage):
         self.dataSize = dataSize
         self.hummPercentage = hummPercentage
 
     def generate(self):
+        #generuje tablicę danych
         for i in range(0, self.dataSize):
-            self.tab.append(random.randint(0, 1))
+            self.tab.append(random.getrandbits(1))
+
+    # def hummm(self):
+    #     for i in range(0, int(self.hummPercentage * self.dataSize)):
+    #         temp = random.randrange(0, self.dataSize)
+    #         self.tab[temp] = -self.tab[temp] + 1
 
     def humm(self):
-        for i in range(0, int(self.hummPercentage * self.dataSize)):
-            temp = random.randrange(0, 100)
-            self.tab[temp] = -self.tab[temp] + 1
+        #zaszumia dane
+        #TODO wybieranko zaszumiania
+
+        #kopiuje tablicę do porównania
+        temp = Data(self.dataSize, self.hummPercentage)
+        temp.tab = copy.deepcopy(self.tab)
+        counter = 0
+
+        #zaszumianie
+        for i in range(0, int(self.dataSize)):
+            if(random.randrange(0,100)<=self.hummPercentage):
+                self.tab[i] = -self.tab[i] + 1
+
+        #obliczanie porcentu błędów
+        for i in range(0, self.dataSize):
+            if (temp.tab[i] != self.tab[i]):
+                counter += 1
+        return counter/self.dataSize
 
     def print(self):
+        #printuje dane
         print(self.tab)
 
--- venv/main.py
+++ venv/main.py
@@ -1,9 +1,7 @@
 from data import Data
+import copy
 
-
-data = Data(1000, 0.3)
-
+data = Data(100000, 0.1)
 data.generate()
 data.print()
-data.humm()
-data.print()
+print(data.humm())
