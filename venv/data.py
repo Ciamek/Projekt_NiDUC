cd50b5ace52e43fdf74839729a9f623f797f2c14
mscoding <37419432+mscoding@users.noreply.github.com>
2020-04-06 22:40:17 +0200

    dudududum

--- venv/data.py
+++ venv/data.py
@@ -3,24 +3,24 @@ import copy
 
 
 class Data:
-    tab=[]              #tablica z danymi
+    bits=[]              #lista z danymi
     dataSize = 0        #długość ciągu bitów
     hummPercentage = 0  #procentowa ilość błędów
 
     random.seed()
-    def __init__(self, dataSize, hummPercentage):
+    def __init__(self,dataSize, hummPercentage):
         self.dataSize = dataSize
         self.hummPercentage = hummPercentage
 
     def generate(self):
         #generuje tablicę danych
         for i in range(0, self.dataSize):
-            self.tab.append(random.getrandbits(1))
+            self.bits.append(random.getrandbits(1))
 
     # def hummm(self):
     #     for i in range(0, int(self.hummPercentage * self.dataSize)):
     #         temp = random.randrange(0, self.dataSize)
-    #         self.tab[temp] = -self.tab[temp] + 1
+    #         self.bits[temp] = -self.bits[temp] + 1
 
     def humm(self):
         #zaszumia dane
@@ -28,21 +28,52 @@ class Data:
 
         #kopiuje tablicę do porównania
         temp = Data(self.dataSize, self.hummPercentage)
-        temp.tab = copy.deepcopy(self.tab)
+        temp.bits = copy.deepcopy(self.bits)
         counter = 0
 
         #zaszumianie
         for i in range(0, int(self.dataSize)):
             if(random.randrange(0,100)<=self.hummPercentage):
-                self.tab[i] = -self.tab[i] + 1
+                self.bits[i] = -self.bits[i] + 1
 
         #obliczanie porcentu błędów
         for i in range(0, self.dataSize):
-            if (temp.tab[i] != self.tab[i]):
+            if (temp.bits[i] != self.bits[i]):
                 counter += 1
         return counter/self.dataSize
 
     def print(self):
         #printuje dane
-        print(self.tab)
+        print(self.bits)
 
+    #kodowanie za pomocą FEC
+    def encode(self):
+        encoded_bits = []
+        for bit in self.bits:
+            for i in range(0, 3):
+                encoded_bits.append(bit)
+        self.bits = encoded_bits
+        self.dataSize *= 3
+
+    def decode(self):
+        decoded_bits = []
+        zeros = 0
+        ones = 0
+
+        for bit in self.bits:
+            zeros = 0
+            ones = 0
+
+            for i in range(0, 3):
+                if bit == 0:
+                    zeros += 1
+                else:
+                    ones += 1
+
+            if zeros > ones:
+                decoded_bits.append(1)
+            else:
+                decoded_bits.append(0)
+
+        self.bits = decoded_bits
+        self.dataSize /= 3
--- venv/main.py
+++ venv/main.py
@@ -4,4 +4,6 @@ import copy
 data = Data(100000, 0.1)
 data.generate()
 data.print()
+data.encode()
+data.print()
 print(data.humm())
